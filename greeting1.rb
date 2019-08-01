b541d5b8d094f64adbd597f57aab53c0620a7520
Catherine Yang <liahnee@gmail.com>
2019-08-01 14:57:49 +0000

    Automatically backed up by Learn

---
+++ greeting1.rb
@@ -0,0 +1,15 @@
+def greeting_a_person(name)
+ puts "Hello #{name}"
+end
+
+greeting_a_person("Maria")
+
+def greeting_programmer(name, language)
+   puts "Hello, #{name}. We heard you are a great #{language} programmer."
+ end
+ 
+ greeting_programmer("Maria", "Ruby")
+ 
+ greeting_programmer("Steven", "Elixir")
+ 
+ greeting_programmer("Ana")
---
+++ learning-if-end.rb
@@ -0,0 +1,24 @@
+#run_code_inside = FALSE
+#puts "Code before if...end"
+#if run_code_inside
+#  puts "code inside"
+#end
+#puts "Code after if...end"
+
+chance_of_rain = 0.2
+puts "Let's gp outside!"
+if chance_of_rain<=0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
+puts "Oh, and always wear sunscreen!"
+
+
+puts "You know what year it is??"
+this_year = Time.now.year
+puts "Hey, it's 2019!" if this_year == 2019
+
+puts "Hey, it's not 2019!" unless this_year==2019
